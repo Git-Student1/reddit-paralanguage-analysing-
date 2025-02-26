909424b2b3e66b6f2c06e6319354dbcdedf04b3e
Maximilian Ehlert <maximilian.ehlert@student.htw-berlin.de>
2025-02-26 02:26:56 +0100

    refactoring: comparing and grouping functionality of last commit

--- comparison.py
+++ comparison.py
@@ -10,10 +10,19 @@ class MultiThreadOperations:
 
     def grouping(self, post_group:list[str]):
         #TODO: remove coupling with compare_groups method
-        pfn = self.masterfile.post_full_name_cn
+        df = self.__get_df_sum(post_group=post_group)
+        title = f"grouping for {post_group}"
+        self.__create_plot_for_df(df=df, title=title)
+
+    def __get_df_sum(self, post_group:list[str]):
+        # only the para language values
         df = self.masterfile.df[self.masterfile.para_all_columns + [self.masterfile.post_full_name_cn]]
+        # only the values from the group 
         df = df[df[self.masterfile.post_full_name_cn].notna() & df[self.masterfile.post_full_name_cn].isin( post_group)]
-        df = pd.DataFrame(df.agg("sum"))
+        return pd.DataFrame(df.agg("sum"))
+    
+    def __create_plot_for_df(self, df:pd.DataFrame, title:str):
+        pfn = self.masterfile.post_full_name_cn
         df.columns = df.columns.astype(str)
         for column in df.columns:
             df.rename({column.__str__(): str(df.loc[pfn, column])}, axis=1, inplace=True, errors='raise')
@@ -21,37 +30,17 @@ class MultiThreadOperations:
         #print(result.to_string())
         ax = df.plot.barh()
         ax.bar_label(ax.containers[0])
-        ax.set_title( f"grouping for {post_group}")
+        ax.set_title(title)
         plt.subplots_adjust(top=0.9, bottom=0.1, left=0.3, right=0.9)
         plt.show()
-
-        
 
     def compare_groups(self, post_group1:list[str], post_group2:list[str]):
-        # only the para language values
-        pfn = self.masterfile.post_full_name_cn
-        df = self.masterfile.df[self.masterfile.para_all_columns + [self.masterfile.post_full_name_cn]]
-        #print(df.to_string())
-        # only the values from the groups to compare 
-        df_group1 = df[df[self.masterfile.post_full_name_cn].notna() & df[self.masterfile.post_full_name_cn].isin( post_group1)]
-        df_group2 = df[df[self.masterfile.post_full_name_cn].isin( post_group2)]
-        print(df_group1.to_string())
-        df_group1_grouped = df_group1.agg("sum")
-        df_group2_grouped = df_group2.agg("sum")
-        #print(df_group1_grouped)
-        #print(df_group2_grouped)
+
+        df_group1_grouped = self.__get_df_sum(post_group=post_group1)
+        df_group2_grouped = self.__get_df_sum(post_group=post_group2)
         result = pd.concat([df_group1_grouped, df_group2_grouped], axis=1)
-        #print(result.columns.tolist())
-        result.columns = result.columns.astype(str)
-        for column in result.columns:
-            result.rename({column.__str__(): str(result.loc[pfn, column])}, axis=1, inplace=True, errors='raise')
-        result = result.drop(pfn)
-        #print(result.to_string())
-        ax = result.plot.barh()
-        ax.bar_label(ax.containers[0])
-        ax.set_title( f"comparison for {post_group1} and {post_group2}")
-        plt.subplots_adjust(top=0.9, bottom=0.1, left=0.3, right=0.9)
-        plt.show()
+        title = f"comparison for {post_group1} and {post_group2}"
+        self.__create_plot_for_df(df=result, title=title)
         
 
 
